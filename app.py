6f9245a82723fac7195936c9c66d9aa18c1a3425
Mannendri Olivares <mannendriolivares@Mannendris-MBP.lan>
2021-05-29 00:00:32 -0400

    Built out 3 routes for index, create, and done templates

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/create')
+def create():
+    return render_template("create.html")
+
+@app.route('/done')
+def create():
+    return render_template("done.html")
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
