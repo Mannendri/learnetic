3b9551b88fa594d4d64622ae2c0cf1797a88c376
Mannendri Olivares <mannendriolivares@Mannendris-MBP.lan>
2021-06-05 11:26:11 -0400

    Added comments for db tasks that need to be done

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=["GET","POST"])
 def index():
+    # ---Display all study sets---
+    # Code below
     return render_template("index.html")
 
 @app.route('/create', methods=["GET","POST"])
 def create():
+    if request.method == 'POST':
+        # ---Create a study set in the db with its title and description---
+        # Code below
+
+        # ---Create a new flashcard in the db for every flashcard that was inputted in the form---
+        # Code below
+        pass
     return render_template("create.html")
 
 if __name__ == '__main__':
